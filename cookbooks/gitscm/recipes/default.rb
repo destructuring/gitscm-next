940b8758e905ef87cd5f00c0893f52e44411d472
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-11 00:25:45 -0700

    get localhost (development) mode ready to run script/travis-ci

--- cookbooks/gitscm/recipes/default.rb
+++ cookbooks/gitscm/recipes/default.rb
@@ -5,11 +5,28 @@ template "#{node[:release_dir]}/config/database.yml" do
   source "database.yml.erb"
 end
 
-link "#{node[:release_dir]}/log" do
-  to "#{node[:project_dir]}/#{node[:log_dir]}"
+if node[:app_env] == "development"
+  directory "#{node[:release_dir]}/log" do
+    mode 0750
+  end
+
+  execute "test log" do
+    command "touch #{node[:release_dir]}/log/test.log"
+    creates "#{node[:release_dir]}/log/test.log"
+  end
+else
+  link "#{node[:release_dir]}/log" do
+    to "#{node[:project_dir]}/#{node[:log_dir]}"
+  end
+
+  execute "service #{node[:app_name]}" do
+    command "sudo ln -nfs /data/#{node[:app_name]}/current/libexec/#{node[:app_name]} /etc/init.d/#{node[:app_name]}"
+    creates "/etc/init.d/#{node[:app_name]}"
+  end
 end
 
-execute "service #{node[:app_name]}" do
-  command "sudo ln -nfs /data/#{node[:app_name]}/current/libexec/#{node[:app_name]} /etc/init.d/#{node[:app_name]}"
-  creates "/etc/init.d/#{node[:app_name]}"
-end unless node[:app_env] == "development"
+execute "app_env log" do
+  command "touch #{node[:release_dir]}/log/#{node[:app_env]}.log"
+  creates "#{node[:release_dir]}/log/#{node[:app_env]}.log"
+end
+
