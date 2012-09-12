2847b663fdc81c3c3a7d972b2f7bd0da7d3abc5e
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-12 15:22:11 -0700

    adjust localdomain node type

--- nodes/localdomain.rb
+++ nodes/localdomain.rb
@@ -1,12 +1,8 @@
-run_list []
+run_list [ 'gitscm' ]
 platform "generic"
 platform_version "0.0.1"
 
 release_dir "#{ENV['PWD']}"
 project_dir "#{ENV['PWD']}"
-gconfig_dir "XXX"
-home_dir ENV['HOME']
+home_dir "#{ENV['HOME']}"
 app_env "development"
-
-app_dir "."
-ruby_loader "rvm-exec"
