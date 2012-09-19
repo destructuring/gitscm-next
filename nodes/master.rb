a916aa9191cd899c2203bc1f69b14081ddc86b57
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-19 12:26:22 -0700

    config master env

--- nodes/master.rb
+++ nodes/master.rb
@@ -1,8 +1,8 @@
-run_list []
+run_list [ 'gitscm' ]
 platform "generic"
 platform_version "0.0.1"
 
 release_dir ENV['PWD']
-project_dir "XXX"
-home_dir "XXX"
+project_dir "/data/gitscm"
+home_dir ENV['HOME']
 app_env "master"
